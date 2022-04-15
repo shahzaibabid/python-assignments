0aac67ec8fd0e7224e6dcc41ce614cb93b28b50f
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-04-15 16:31:18 +0500

    Assignment3(2)

---
+++ Assignment3(2).py
@@ -0,0 +1,10 @@
+num = int(input("Enter the shopped amount to check the discount!"))
+Amount = 25000
+percent = 15
+dis = (percent*Amount)/100;
+
+if num>= 25000:
+  print("The discount you get on your shop amount is:", dis);
+
+else:
+   print("sorry the shoped amount is not enough to get the discount!")  
