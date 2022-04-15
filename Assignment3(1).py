62415cd222ea5e9fd54a0b0503ddc1e6fb8f62fd
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-04-15 16:00:08 +0500

    Assignment3(1)

---
+++ Assignment3(1).py
@@ -0,0 +1,7 @@
+
+num = int(input("Enter a number to check wheather it is even or odd"))
+var = num % 2
+if var > 0:
+    print("The entered number is an odd number")
+else:
+    print("The entered number is an even number")
